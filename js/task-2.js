f148afa98290e045007f4a2d74b5696d64f4fe99
Andrii_Babenko <andrey.brnr@gmail.com>
2020-12-28 14:32:43 +0200

    task-2 DONE

--- js/task-2.js
+++ js/task-2.js
@@ -5,6 +5,21 @@
 // followers - число
 // Добавь метод getInfo(), который, выводит строку: User ${имя} is ${возраст} years old and has ${кол-во фоловеров} followers
 
+class User {
+  constructor({ name, age, followers }) {
+    this.name = name;
+    this.age = age;
+    this.followers = followers;
+  }
+
+  getInfo() {
+
+    const userInfo = `User ${this.name} is ${this.age} years old and has ${this.followers} followers`
+    return console.log(userInfo); 
+  }
+}
+
+
 const mango = new User({
   name: 'Mango',
   age: 2,
