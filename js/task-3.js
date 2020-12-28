8ffec8949a638e56daaa97f294c2560b1054ced0
Andrii_Babenko <andrey.brnr@gmail.com>
2020-12-28 21:23:47 +0200

    task-3 DONE

--- js/task-3.js
+++ js/task-3.js
@@ -5,6 +5,37 @@
 // getItems() - возвращает массив текущих товаров
 // addItem(item) - получает новый товар и добавляет его к текущим
 // removeItem(item) - получет товар и, если он есть, удаляет его из текущих
+
+class Storage {
+  constructor(items) {
+    this._items = items;
+  }
+  
+  getItems() {
+    return this._items;
+  }
+
+  addItem(item) {
+    this._items.push(item);
+       
+  }
+
+  removeItem(removedItem) {
+
+      const newItems = [];
+
+      for (const item of this._items) {
+
+        if (item === removedItem) continue;
+         
+        newItems.push(item);
+      }
+      this._items = newItems;
+    }  
+    // const newItems = this._items.filter(item => item !== this.removedItem);
+    // this._items = newItems;
+}
+
 const storage = new Storage([
   'Нанитоиды',
   'Пролонгер',
@@ -16,7 +47,8 @@ const items = storage.getItems();
 console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
 
 storage.addItem('Дроид');
-console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+console.table(storage.getItems()); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
 
 storage.removeItem('Пролонгер');
-console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
+console.table(storage.getItems()); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
